5fdcf0a32742e4a436becf34b1aa443d4e618f18
SSOMHEO <hsm8509@gmail.com>
2025-11-07 14:34:32 +0900

    first commit

---
+++ src/DB_conntection.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+import boto3
+import io
+import pymysql
+import json
+from datetime import datetime, timedelta
+from sshtunnel import SSHTunnelForwarder
+
+class SSHMySQLConnector:
+    def __init__(self):
+        self.ssh_host = None
+        self.ssh_username = None
+        self.ssh_password = None
+        self.db_username = None
+        self.db_password = None
+        self.db_name = None
+        self.tunnel = None
+        self.connection = None
+
+    def load_config_from_json(self, json_path):
+        try:
+            with open(json_path, 'r', encoding='utf-8') as f:
+                config = json.load(f)
+                self.ssh_host = config['ssh_host']
+                self.ssh_username = config['ssh_username']
+                self.ssh_password = config['ssh_password']
+                self.db_username = config['db_username']
+                self.db_password = config['db_password']
+                self.db_name = config['db_name']
+        except Exception as e:
+            print("설정 JSON 로딩 실패:", e)
+
+    def connect(self, insert=False):
+        try:
+            self.tunnel = SSHTunnelForwarder(
+                (self.ssh_host, 22),
+                ssh_username=self.ssh_username,
+                ssh_password=self.ssh_password,
+                remote_bind_address=('127.0.0.1', 3306),
+            )
+            self.tunnel.start()
+            # insert 여부에 따라 cursorclass 설정
+            connect_kwargs = {
+                'host': '127.0.0.1',
+                'port': self.tunnel.local_bind_port,
+                'user': self.db_username,
+                'password': self.db_password,
+                'db': self.db_name,
+            }
+            if insert:
+                connect_kwargs['cursorclass'] = pymysql.cursors.DictCursor
+            self.connection = pymysql.connect(**connect_kwargs)
+            print("DB 접속 성공")
+        except Exception as e:
+            print("SSH 또는 DB 연결 실패:", e)
+
+    def execute_query(self, query):
+        # 쿼리 실행 후 데이터를 DataFrame으로 반환
+        return pd.read_sql_query(query, self.connection)
+
+    def insert_query_with_lookup(self, table_name, data_list):
+        try:
+            with self.connection.cursor() as cursor:
+                for data in data_list:
+                    # 1. op_member에서 uid, user_id 조회
+                    cursor.execute("""
+                        SELECT uid, user_id, add1_connected FROM op_member
+                        WHERE add1 = %s
+                        LIMIT 1
+                    """, (data['acnt_nm'],))
+                    result = cursor.fetchone()
+                    
+                    if result:
+                        data['member_uid'] = result['uid']
+                        data['user_id'] = result['user_id']
+                        data['is_connected'] = result['add1_connected']
+                        # 향후에 ig_user_id가 추가가 된다면, 해당 부분도 확인해서 추가할 수 있게
+                        # data['ig_user_id'] = result['ig_user_id']
+                    else:
+                        data['member_uid'] = 0
+                        data['user_id'] = 'None'
+                        data['is_connected'] = 'n'
+                        # data['ig_user_id'] = 'None'
+              
+
+                    # 2. INSERT 쿼리 구성 및 실행
+                    columns = ', '.join(data.keys())
+                    placeholders = ', '.join([f"%({k})s" for k in data.keys()])
+                    insert_sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+                    # print(insert_sql)
+                    cursor.execute(insert_sql, data)
+
+                    print(f"✅ inserted acnt_id: {data.get('acnt_id', 'N/A')}")
+
+            self.connection.commit()
+        except Exception as e:
+            self.connection.rollback()
+            print("INSERT 실패:", e)
+    
+    def close(self):
+        if self.connection:
+            self.connection.close()
+        if self.tunnel:
+            self.tunnel.stop()
+
+def sendQuery(query):
+        ssh = SSHMySQLConnector()
+        ssh.load_config_from_json('/home/flexmatch_ftp/ftp/multimodal_categorizer/config/accounts.json')
+        ssh.connect()
+        results = ssh.execute_query(query)
+        # print(results)
+        # print(results.head())
+        ssh.close()
+
+        return results
+
+def get_all_infos(): 
+    query_user_main_category_info = """
+            SELECT
+            o.user_id, s.acnt_id, s.acnt_nm, s.main_category, s.top_3_category
+            FROM op_member o
+            left join INSTAGRAM_USER_CATEGORY_LABELING s
+            on o.add1=s.acnt_nm
+            where (o.add1 != '' and o.add1 is not null)
+        """
+    user_main_category_info = sendQuery(query_user_main_category_info)
+
+    return user_main_category_info
